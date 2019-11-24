0450b07a548fdc34e749747bc2e229a2d88a3a97
RobertoCastelli <roberto.castelli.teal@gmail.com>
2019-11-24 15:00:27 +0100

    optimized. rotateY to be correct

--- app.js
+++ app.js
@@ -1,61 +1,28 @@
-/**
- * TODO
- * animation
- * sounds
- * starting image
- */
-
-let btnRock = document.getElementById('btnRock');
-let btnPaper = document.getElementById('btnPaper');
-let btnScissors = document.getElementById('btnScissors');
 let imgRight = document.getElementById('img_right');
 let imgLeft = document.getElementById('img_left');
 let message = document.getElementById('game_message');
 let scoreLeft = document.getElementById('score_left');
 let scoreRight = document.getElementById('score_right');
-let buttons = document.getElementsByTagName('button');
-
-let rock = 'img/rock.png';
-let paper = 'img/paper.png';
-let scissors = 'img/scissors.png';
-
-let computerNumber;
+let buttons = document.querySelectorAll('.buttons button');
+let hands = document.querySelectorAll('.hands img');
 let playerChoice;
-let imageArray = [rock, paper, scissors];
+let computeChoice;
 
-let scoreL = 0;
-let scoreR = 0;
 
-imgLeft.src = 'img/left-hand.png';
-imgRight.src = 'img/right-hand.png';
+init();
 
-btnRock.addEventListener('click', () => {
-    countdown();
-    disableButtons(true);
-    setTimeout(() => {
-        playerChoice = rock;
-        imgRight.src = rock;
-        disableButtons(false);
-    }, 3050);
-});
-
-btnPaper.addEventListener('click', () => {
-    countdown();
-    disableButtons(true);
-    setTimeout(() => {
-        playerChoice = paper;
-        imgRight.src = paper;
-        disableButtons(false);
-    }, 3050);
-});
-
-btnScissors.addEventListener('click', () => {
-    countdown();
-    disableButtons(true);
-    setTimeout(() => {
-        playerChoice = scissors;
-        imgRight.src = scissors;
-        disableButtons(false);
-    }, 3050);
+buttons.forEach(choice => {
+    choice.addEventListener('click', (event) => {
+        count();
+        setTimeout(() => {
+            playerChoice = event.target.textContent;
+            imgRight.src = `img/${playerChoice}.png`;
+            computerSelection();
+            checkHands();
+            updateScore();
+            stopCount();
+            endGame();
+        }, 3000);
+    })
 });
 
--- functions.js
+++ functions.js
@@ -1,62 +1,65 @@
-function computerChoice() {
-    computerNumber = Math.floor(Math.random() * 3);
-    imgLeft.src = imageArray[computerNumber]
+function computerSelection() {
+    const computerOption = ['rock', 'paper', 'scissors'];
+    const computerNumber = Math.floor(Math.random() * 3);
+    computerChoice = computerOption[computerNumber];
+    imgLeft.src = `img/${computerChoice}.png`;
 };
 
-function score() {
-    if (playerChoice == imageArray[computerNumber]) {
-        message.innerHTML = 'TIE! - try again';
-    } else if (playerChoice == rock && computerNumber == 2 || playerChoice == paper && computerNumber == 0 || playerChoice == scissors && computerNumber == 1) {
-        message.innerHTML = 'YOU WIN! - try again';
-        scoreR++
-        scoreRight.innerHTML = scoreR;
+function checkHands() {
+    if (playerChoice == computerChoice) {
+        message.innerHTML = 'TIE - try again';
+    } else if ( playerChoice == 'rock' && computerChoice == 'scissors' || playerChoice == 'paper' && computerChoice == 'rock' || playerChoice == 'scissors' && computerChoice == 'paper') {
+        message.innerHTML = 'You WIN - try again';
+        scoreR++;
     } else {
-        message.innerHTML = 'YOU LOSE! - try again';
-        scoreL++
-        scoreLeft.innerHTML = scoreL;
+        message.innerHTML = 'You LOSE - try again';
+        scoreL++;
     }
+};
+
+function updateScore() {
+    scoreRight.innerHTML = scoreR;
+    scoreLeft.innerHTML = scoreL;
 };
 
 function endGame() {
-    if (scoreL == 3 || scoreR == 3) {
-        btnPaper.disabled = true;
-        btnRock.disabled = true;
-        btnScissors.disabled = true;
-        message.innerHTML = 'GAME OVER';
+    if (scoreL == 3) {
+        message.innerHTML = 'Computer WINS';
+        disableButtons(true);
         setTimeout(() => {
-            message.innerHTML = `the score is: Computer ${scoreL} - Player ${scoreR}`
-        }, 2000);
+            init();
+        }, 3000);
+    } else if (scoreR == 3) {
+        message.innerHTML = 'Player WINS';
+        disableButtons(true);
         setTimeout(() => {
-            window.location.reload();
-        }, 4000);
+            init();
+        }, 3000);
     }
 };
 
-function countdown() {
-    let n = 3;
-    count();
-    function count() {
-        imgLeft.classList.add('mover');
-        imgRight.classList.add('mover');
-        imgRight.src = 'img/right-hand.png';
-        imgLeft.src = 'img/left-hand.png';
-        message.innerHTML = n;
-        if (n == 0) {
-            computerChoice();
-            score();
-            endGame();
-            imgLeft.classList.remove('mover');
-            imgRight.classList.remove('mover');
-        } else {
-            n -= 1;
-            setTimeout(count, 1000);
-        }
-    }
-};
+function init() {
+    imgLeft.src = 'img/rock.png';
+    imgRight.src = 'img/rock.png';
+    message.innerHTML = 'choose your play'
+    scoreL = 0;
+    scoreR = 0;
+    updateScore();
+    disableButtons(false);
+};   
 
 function disableButtons(bool) {
-    for (i = 0; i < buttons.length; i++) {
-        buttons[i].disabled = bool;
-    }
-}
+    buttons.forEach(button => button.disabled = bool);
+};
 
+function count() {
+    imgLeft.src = 'img/rock.png';
+    imgRight.src = 'img/rock.png';
+    hands.forEach(hand => hand.classList.add('shaker'));
+    disableButtons(true);
+};
+
+function stopCount() {
+    hands.forEach(hand => hand.classList.remove('shaker'));
+    disableButtons(false);
+};
